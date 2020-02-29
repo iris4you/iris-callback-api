6baf892221f563e91990f659c34a795cbda1c6e6
S1S13AF7 <Administrator@S1>
2020-02-29 10:17:05 +0200

    Дружба с дежурным по сигналу (.д +др)

--- classes/Ub/Callback/SendSignal.php
+++ classes/Ub/Callback/SendSignal.php
@@ -20,14 +20,25 @@ class UbCallbackSendSignal implements UbCallbackAction {
 		}
 
 		if ($in == 'др' || $in == '+др' || $in == '+друг' || $in  == 'дружба' || $in  == '+дружба') {
-				$res = $vk->vkRequest('friends.add', 'user_id='.$id);
+				$get = $vk->vkRequest('friends.areFriends', 'user_ids='.$id);
+				$are = (int)@$get['response'][0];
+				if ($are == 3) {
+						$msg = UB_ICON_SUCCESS . ' ok';
+				} elseif ($are == 1) {
+						$msg =  UB_ICON_INFO . ' отправлена заявка/подписка пользователю @id' . $id;
+				} else {
+						$res = $vk->vkRequest('friends.add', 'user_id='.$id);
 				if (isset($res["response"])) {
 						$msg = UB_ICON_SUCCESS . ' ok';
+						if ($res["response"] == 1) $msg =  UB_ICON_INFO . ' отправлена заявка пользователю @id' . $id;
+						if ($res["response"] == 2) $msg =  UB_ICON_SUCCESS . ' заявка одобрена';
+						if ($res["response"] == 4) $msg =  UB_ICON_WARN . ' повторная отправка заявки';
 				} elseif($r = $res["error"]) {
 						$msg = UB_ICON_WARN . $r["error_msg"];
 				if ($r["error_code"] == 174) $msg = UB_ICON_WARN . ' ВК не разрешает дружить с собой';
 				if ($r["error_code"] == 175) $msg = UB_ICON_WARN . ' Удилите дежурного из ЧС! ' . UB_ICON_WARN; 
 				if ($r["error_code"] == 176) $msg = UB_ICON_WARN . ' Вы в ЧС у дежурного (наверное за дело?!)'; }
+				}
 				$vk->chatMessage($chatId, $msg);
 				echo 'ok';
 				return;
