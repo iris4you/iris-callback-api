04197a82121f2aed008d1b2151d200eea3335c7a
S1S13AF7 <>
2020-02-29 09:32:31 +0200

    Дружба с дежурным по сигналу (.д +др)

--- classes/Ub/Callback/SendSignal.php
+++ classes/Ub/Callback/SendSignal.php
@@ -9,11 +9,26 @@ class UbCallbackSendSignal implements UbCallbackAction {
 
 		$vk = new UbVkApi($userbot['token']);
 		$in = $object['value']; // сам сигнал
+		$id = $object['from_id']; // от кого
 
 		if ($in == 'ping' || $in == 'пинг'  || $in == 'пінг'  || $in == 'пінґ') {
 				$getVkTime = $vk->vkRequest('utils.getServerTime',''); /* надо токен */
 				$time = (isset($getVkTime["response"])) ? $getVkTime["response"]:time();
 				$vk->chatMessage($chatId, "PONG\n" .($time - $message['date']). " сек");
+				echo 'ok';
+				return;
+		}
+
+		if ($in == 'др' || $in == '+др' || $in == '+друг' || $in  == 'дружба' || $in  == '+дружба') {
+				$res = $vk->vkRequest('friends.add', 'user_id='.$id);
+				if (isset($res["response"])) {
+						$msg = UB_ICON_SUCCESS . ' ok';
+				} elseif($r = $res["error"]) {
+						$msg = UB_ICON_WARN . $r["error_msg"];
+				if ($r["error_code"] == 174) $msg = UB_ICON_WARN . ' ВК не разрешает дружить с собой';
+				if ($r["error_code"] == 175) $msg = UB_ICON_WARN . ' Удилите дежурного из ЧС! ' . UB_ICON_WARN; 
+				if ($r["error_code"] == 176) $msg = UB_ICON_WARN . ' Вы в ЧС у дежурного (наверное за дело?!)'; }
+				$vk->chatMessage($chatId, $msg);
 				echo 'ok';
 				return;
 		}
