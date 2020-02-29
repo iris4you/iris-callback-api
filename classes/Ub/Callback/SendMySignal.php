895acf2526f36475c9a72a09d2270e5f23c1c8fa
S1S13AF7 <>
2020-02-29 09:36:18 +0200

    с. чек_собак, с. бан_собак (на профиле, не в чате)

--- classes/Ub/Callback/SendMySignal.php
+++ classes/Ub/Callback/SendMySignal.php
@@ -18,6 +18,46 @@ class UbCallbackSendMySignal implements UbCallbackAction {
 				return;
 		}
 
+		if ($in == 'check_dogs' || $in == 'чек_собак' || $in == 'бан_собак') {
+				$raw = $vk->vkRequest('friends.get', "count=5000&fields=deactivated");
+				$count = (int)@$raw["response"]["count"];
+				$dogs = 0;
+				$msg = '';
+		if ($count && isset($raw["response"]["items"])) {
+				$items = $raw["response"]["items"];
+
+        foreach ($items as $user) {
+            $id = $user['id'];
+            $name = (string) @$user["first_name"] .' ' . (string) @$user["last_name"];
+            $deactivated = (string)@$user["deactivated"];
+
+            if ($deactivated) {
+                $dogs++;
+            if ($in === 'бан_собак') {
+
+                $del = $vk->vkRequest('friends.delete', 'user_id='.$id);
+		                            sleep(1); 
+                $ban = $vk->vkRequest('account.ban', 'owner_id='.$id);
+		                            sleep(1); 
+            }
+                $fr = UB_ICON_WARN . " ($deactivated)";
+                $msg.=UB_ICON_WARN . " [id$id|$name] $fr\n";
+
+            }
+
+            
+            
+         }
+    }
+
+		if(!$dogs) {
+				$msg = 'НЕМА'; } elseif($in == 'бан_собак') {
+				$msg.= "спробували видалити $dogs собак (а чи вийшло перевірте)"; }
+				$vk->chatMessage($chatId, $msg, ['disable_mentions' => 1]);
+				echo 'ok';
+				return;
+		}
+
 		$vk->chatMessage($chatId, UB_ICON_WARN . ' ФУНКЦИОНАЛ НЕ РЕАЛИЗОВАН');
 		echo 'ok';
 	}
