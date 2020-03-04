05d8c37449a26c5c2f32dee8a22172743a4a029a
S1S13AF7 <>
2020-03-04 12:32:47 +0200

    Просили сделать. пришлось… не особо красиво... но работает.

--- classes/Ub/Callback/SendMySignal.php
+++ classes/Ub/Callback/SendMySignal.php
@@ -29,6 +29,45 @@ class UbCallbackSendMySignal implements UbCallbackAction {
 				return;
 		}
 
+		if ($in == 'др' || $in == '+др' || $in == '+друг' || $in  == 'дружба' || $in  == '+дружба') {
+				$cmi = $object['conversation_message_id'];
+				$res = $vk->messagesGetByConversationMessageId(UbVkApi::chat2PeerId($chatId), $cmi);
+				$fwd = $res["response"]["items"][0]["fwd_messages"];
+				if(!count($fwd)) {
+				$vk->chatMessage($chatId, UB_ICON_WARN . ' Не нашёл сообщений');
+				echo 'ok';
+				return; } elseif(count($fwd) > 7) {
+				$vk->chatMessage($chatId, UB_ICON_WARN . ' Многабукаф,ниасилил');
+				echo 'ok';
+				return; }
+				$msg = '';
+				foreach($fwd as $m) {
+				$ids[$m["from_id"]]=$m["from_id"]; /* исключаем случаи, когда несколько пересланных от одного id */
+				}
+
+				$cnt = 0;
+
+				foreach($ids as $id) {
+								$cnt++;
+								$are = $vk->AddFriendsById($id);
+						if ($are == 3) {
+								$msg.= UB_ICON_SUCCESS . " @id$id ok\n";
+						} elseif ($are == 1) {
+								$msg.=  UB_ICON_INFO . " отправлена заявка/подписка пользователю @id$id\n";
+						} elseif ($are == 2) {
+								$msg.=  UB_ICON_SUCCESS . " заявка от @id$id одобрена\n";
+						} elseif ($are == 4) {
+								$msg.=  UB_ICON_WARN . " повторная отправка заявки @id$id\n";
+						} elseif(is_array($are)) {
+								$msg.= UB_ICON_WARN . " $are[error_msg]\n"; 
+						}
+								sleep($cnt);
+				}
+				$vk->chatMessage($chatId, $msg, ['disable_mentions' => 1]);
+				echo 'ok';
+				return;
+		}
+
 		if ($in == 'прийом') {
 				$add = $vk->confirmAllFriends();
 				$msg = $add ? '+'.$add : 'НЕМА';
