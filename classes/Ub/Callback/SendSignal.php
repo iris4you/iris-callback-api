9c2876b8d09d43e85d529aff2e0fb21aec2e4ad4
S1S13AF7 <>
2021-09-14 13:33:34 +0300

    прислали сигнал. От

--- classes/Ub/Callback/SendSignal.php
+++ classes/Ub/Callback/SendSignal.php
@@ -1,5 +1,26 @@
 <?php
+
+/**
+ * @const TIME_START Время запуска скрипта в миллисекундах
+ */
+	if(!defined('TIME_START')) {
+	    define ('TIME_START', microtime(true)); // время запуска скрипта
+	}
+
 class UbCallbackSendSignal implements UbCallbackAction {
+
+	function closeConnection() {
+		@ob_end_clean();
+		@header("Connection: close");
+		@ignore_user_abort(true);
+		@ob_start();
+		echo 'ok';
+		$size = ob_get_length();
+		@header("Content-Length: $size");
+		@ob_end_flush(); // All output buffers must be flushed here
+		@flush(); // Force output to client
+	}
+
 	function execute($userId, $object, $userbot, $message) {
 		$chatId = UbUtil::getChatId($userId, $object, $userbot, $message);
 		if (!$chatId) {
@@ -7,8 +28,71 @@ class UbCallbackSendSignal implements UbCallbackAction {
 			return;
 		}
 
+		self::closeConnection();
+
 		$vk = new UbVkApi($userbot['token']);
-		$vk->chatMessage($chatId, 'Мне прислали сигнал. От пользователя @id' . $object['from_id'], ['disable_mentions' => 1]);
+		$in = $object['value']; // сам сигнал
+		$id = $object['from_id']; // от кого
+		$time = time(); # время этого сервера
+		#time = $vk->getTime(); // ServerTime
+		$tag = ($id<0)?'@club'.(-1 * $id):'@id'.$id; /* упонание @ */
+		$options = ['disable_mentions' => 1,'dont_parse_links' => 1];
+		$CanCtrl = (bool)(preg_match("#$id#ui",@$userbot['access']));
+		if ((int)@$object['from_id'] == (int)$userId)$CanCtrl = True;
+
+
+		$vk->chatMessage($chatId, 'Мне прислали сигнал. От пользователя ' . $tag, ['disable_mentions' => 1]);
 	}
+
+    static function for_name($text) {
+        return trim(preg_replace('#[^\pL0-9\=\?\!\@\\\%/\#\$^\*\(\)\-_\+ ,\.:;]+#ui', '', $text));
+    }
+
+    static function number($num, $one, $two, $more) {
+        $num = (int)$num;
+        $l2 = substr($num, strlen($num) - 2, 2);
+
+        if ($l2 >= 5 && $l2 <= 20)
+            return $more;
+        $l = substr($num, strlen($num) - 1, 1);
+        switch ($l) {
+            case 1:
+                return $one;
+                break;
+            case 2:
+                return $two;
+                break;
+            case 3:
+                return $two;
+                break;
+            case 4:
+                return $two;
+                break;
+            default:
+                return $more;
+                break;
+        }
+    }
+
+    /**
+     * Получение подстроки
+     * Корректная работа с UTF-8
+     * @param string $text Исходная строка
+     * @param integer $len Максимальная длина возвращаемой строки
+     * @param integer $start Начало подстроки
+     * @param string $mn Текст, подставляемый в конец строки при условии, что возхвращаемая строка меньще исходной
+     * @return string
+     */
+    function substr($text, $len, $start = 0, $mn = '') {
+        $text = trim($text);
+        if (function_exists('mb_substr')) {
+            return mb_substr($text, $start, $len) . (mb_strlen($text) > $len - $start ? $mn : null);
+        }
+        if (function_exists('iconv')) {
+            return iconv_substr($text, $start, $len) . (iconv_strlen($text) > $len - $start ? $mn : null);
+        }
+
+        return $text;
+    }
 
 }
